11bf9e1ead1bb4bc46b3f1643741769aa5855c6d
shubham <shubham.navale@coradius.io>
2020-02-07 23:37:13 +0530

    Testing widgets

--- components/widgets/PasswordWidget.js
+++ components/widgets/PasswordWidget.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Text, View } from 'react-native'
 import PropTypes from "prop-types";
 
 function PasswordWidget(props) {
   const { BaseInput } = props.registry.widgets;
-  return <BaseInput type="password" {...props} />;
+  return <>
+  <View><Text>Test</Text></View>
+    <BaseInput type="password" {...props} />
+  </>
 }
 
 if (process.env.NODE_ENV !== "production") {
