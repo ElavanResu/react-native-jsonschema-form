8f2edbcc5418a942c8b828368c07fc289a267393
shubham <shubham.navale@coradius.io>
2020-02-08 00:26:40 +0530

    Added react-native-paper & react-native-vector-icons. Changed UI for TextWidget to test

--- components/widgets/TextWidget.js
+++ components/widgets/TextWidget.js
@@ -1,16 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
+import { TextInput } from 'react-native-paper'
 
 function TextWidget(props) {
   const { BaseInput } = props.registry.widgets;
-  return <BaseInput {...props} />;
-}
-
-if (process.env.NODE_ENV !== "production") {
-  TextWidget.propTypes = {
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    id: PropTypes.string,
-  };
+  console.log('props of TextWidget: ', props)
+  return (
+    <TextInput
+      error={showError}
+      mode='outlined'
+      readOnly={readonly}
+      disabled={disabled}
+      keyboardType={inputType === 'number' ? 'phone-pad' : ''}
+      autoFocus={autofocus}
+      value={(value === null || isNotDefined(value)) ? '' : `${value}`}
+      // {...inputProps}
+      // onChangeText={_onChange}
+      // onBlur={onBlur && (event => onBlur(inputProps.id, event.target.value))}
+      // onFocus={onFocus && (event => onFocus(inputProps.id, event.target.value))}
+    />
+  )
 }
 
 export default TextWidget;
